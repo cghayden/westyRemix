84f459e92f22ab91e5955fb424030e74e62af97c
Corey Hayden <cghayden@gmail.com>
2022-03-09 13:15:06 -0500

    featured section flex

--- app/components/FeaturedItems.tsx
+++ app/components/FeaturedItems.tsx
@@ -3,11 +3,15 @@ import CoffeeCard from './CoffeeCard';
 
 function FeaturedItems({ allCoffee }: { allCoffee: Coffee[] }) {
   return (
-    <div className='p-4'>
-      <h2>FeaturedItems</h2>
-      {allCoffee.map((coffee) => (
-        <CoffeeCard coffee={coffee} />
-      ))}
+    <div className='p-4 w-full flex flex-col items-center '>
+      <div>
+        <h2>FeaturedItems</h2>
+      </div>
+      <div className='flex flex-col w-full  justify-center md:flex-row'>
+        {allCoffee.map((coffee) => (
+          <CoffeeCard coffee={coffee} />
+        ))}
+      </div>
     </div>
   );
 }
