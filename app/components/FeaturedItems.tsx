85eae231a49be5eb9bde3e6418bcd9ca792fbd08
Corey Hayden <cghayden@gmail.com>
2022-03-09 15:56:08 -0500

    flex, wrap and center on featured items section

--- app/components/FeaturedItems.tsx
+++ app/components/FeaturedItems.tsx
@@ -5,9 +5,9 @@ function FeaturedItems({ allCoffee }: { allCoffee: Coffee[] }) {
   return (
     <div className='p-4 w-full flex flex-col items-center '>
       <div>
-        <h2>FeaturedItems</h2>
+        <h2 className='text-2xl font-medium'>Featured Items Heading</h2>
       </div>
-      <div className='flex flex-col w-full  justify-center md:flex-row'>
+      <div className='flex flex-wrap justify-center'>
         {allCoffee.map((coffee) => (
           <CoffeeCard coffee={coffee} />
         ))}
