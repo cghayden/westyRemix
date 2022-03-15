b11ddc3e7da532b099e9b8e2962e4f5c85eaf498
Corey Hayden <cghayden@gmail.com>
2022-03-15 14:31:47 -0400

    include files left out of previous commit :: Background color/img query in root layout, overscroll-behavior:none to hide mismatched bg-color above body

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,9 @@
 import { LoaderFunction, useLoaderData } from 'remix';
 import sanity from '~/utils/sanity';
-import type { Coffee, LandingPage, SiteSettings } from '../../sanityTypes';
+import type { Coffee, LandingPage } from '../../sanityTypes';
 
 import HomeHero from '~/components/HomeHero';
 import FeaturedItems from '~/components/FeaturedItems';
-
-const siteSettingsQuery = `*[_type == "siteSettings"][0] {
-  backgroundColor,
-  textColor
-} `;
 
 const contentQuery = `{
   "heroContent": *[_id == "homePage" ] {
@@ -26,14 +21,11 @@ const contentQuery = `{
 interface LoaderData {
   coffee: Coffee[];
   heroContent: LandingPage;
-  siteSettings: SiteSettings;
 }
 
 export const loader: LoaderFunction = async () => {
-  const siteSettings: SiteSettings = await sanity.fetch(siteSettingsQuery);
   const content = await sanity.fetch(contentQuery);
   const data: LoaderData = {
-    siteSettings,
     coffee: content.coffee,
     heroContent: content.heroContent,
   };
@@ -42,11 +34,10 @@ export const loader: LoaderFunction = async () => {
 
 export default function Index() {
   const data = useLoaderData<LoaderData>();
-  console.log('data', data);
   return (
     <div>
       <HomeHero heroContent={data.heroContent} />
-      <FeaturedItems allCoffee={data.coffee} siteSettings={data.siteSettings} />
+      <FeaturedItems allCoffee={data.coffee} />
     </div>
   );
 }
