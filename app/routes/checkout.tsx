71e03019f6de0106b63594eb80b7acbbf1a4001c
Corey Hayden <cghayden@gmail.com>
2022-08-31 21:57:57 -0400

    show summary header separated

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -7,32 +7,43 @@ export default function CheckoutPage() {
   const cartItems = useCartItems();
   const [showSummary, toggleShowSummary] = useState(false);
   return (
-    <div className='flex p-2'>
-      <button
-        className='self-start p-2'
-        onClick={() => toggleShowSummary(!showSummary)}
-      >
-        <div className={`${showSummary ? 'rotate-90' : ''}`}>
-          <PlaySvg />
-        </div>
-      </button>
-      <p className='p-2'>
-        {showSummary ? (
-          <ul className='text-center '>
-            {cartItems.map((cartItem) => (
-              <li key={cartItem.coffeeName}>
-                {`${cartItem.quantity} ${cartItem.coffeeName}, ${
-                  cartItem.grind
-                }: $${formatMoney(cartItem.price * cartItem.quantity)}`}
+    <>
+      <div className='flex p-2 w-full items-center '>
+        <button
+          className='self-start p-2'
+          onClick={() => toggleShowSummary(!showSummary)}
+        >
+          <div className={`${showSummary ? 'rotate-90' : ''}`}>
+            <PlaySvg />
+          </div>
+        </button>
+        <p>{showSummary ? `Hide Summary` : `Show Summary`}</p>
+        <p className='ml-auto'>T?.00</p>
+      </div>
+      <div>
+        <p className='p-2'>
+          {showSummary ? (
+            <ul className='text-center '>
+              {cartItems.map((cartItem) => (
+                <li className='px-3' key={cartItem.coffeeName}>
+                  <p className='flex'>
+                    {`${cartItem.quantity} ${cartItem.coffeeName}, ${cartItem.grind}: `}
+                    <span className='ml-auto'>
+                      {`$${formatMoney(cartItem.price * cartItem.quantity)}`}
+                    </span>
+                  </p>
+                </li>
+              ))}
+              <li className='px-3'>
+                <p className='flex'>
+                  Shipping:
+                  <span className='ml-auto'>$?.00</span>
+                </p>
               </li>
-            ))}
-            <li>Shipping: $?.00</li>
-            <li>Total: ?.00</li>
-          </ul>
-        ) : (
-          <span>Show Summary</span>
-        )}
-      </p>
-    </div>
+            </ul>
+          ) : null}
+        </p>
+      </div>
+    </>
   );
 }
