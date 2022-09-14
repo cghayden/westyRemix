43f3e6245663272a7b12f13538f10c82a0598b7e
Corey Hayden <cghayden@gmail.com>
2022-09-14 09:02:47 -0400

    checkout: query sanity for prices and calc total for paymentIntent, calcVerifiedTotal function, moved collapsible cart summary to ots own component

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -1,56 +1,60 @@
 //https://stripe.com/docs/stripe-js/react
 //The Elements provider wraps any subcomponents that render Stripe Elements. It expects a stripe prop and since we’re using the PaymentElement, we also need to pass an options prop.
 import { loadStripe } from '@stripe/stripe-js';
-import { Form, useActionData } from '@remix-run/react';
 import { Elements, PaymentElement } from '@stripe/react-stripe-js';
-import { createPaymentIntent } from '~/lib/stripePaymentIntent';
+import { Form, useActionData } from '@remix-run/react';
 import { ActionArgs } from '@remix-run/node';
 import invariant from 'tiny-invariant';
-import { useCartItems } from '~/context/useCart';
-import { useState } from 'react';
-import PlaySvg from '~/icons/PlaySvg';
-import formatMoney from '~/lib/formatMoney';
-import type { CartItem } from '../../myTypes';
-
+import { createPaymentIntent } from '~/lib/stripePaymentIntent';
 import sanityClient from '~/lib/sanity/sanity';
-import { Coffee } from 'sanityTypes';
 import reduceCartByName from '~/lib/reduceCartByName';
+import type { KeyedCartItem } from '../../myTypes';
+import { Coffee } from 'sanityTypes';
+import calcVerifiedTotal from '~/lib/calcVerifiedTotal';
+
+//TODO - what if item is out of stock or deleted when sanity queries cart coffees?
 
 const stripePromise = loadStripe('pk_test_CkfBPTwVc1IMB6BXSDsSytR8');
 
 export const action = async ({ request }: ActionArgs) => {
   const body = await request.formData();
+  //1. get cart from form submitted by reviewCart.tsx
   const res = body.get('cart');
   invariant(
     typeof res === 'string',
     'cart not submitted properly; must be a string'
   );
   const cart = JSON.parse(res);
-  //make array of names of coffee in cart
-  const cartObjKeyedByName = reduceCartByName(cart);
-  console.log('cartObjKeyedByName', cartObjKeyedByName);
-  const coffeeInCart: string[] = cart.map((coffee: CartItem) => coffee.name);
 
-  // const sanityQuery = `*[_type == "coffee" && name in ${JSON.stringify(
-  //   coffeeInCart
-  // )} && !(_id in path("drafts.**"))]
-  // `;
-  // const currentCoffeePrices: Coffee[] = await sanityClient.fetch(sanityQuery);
-  // const coffeePrices = currentCoffeePrices.map((coffee) => [
-  //   coffee.name,
-  //   coffee.price,
-  // ]);
-  // const coffeePriceObj = Object.fromEntries(coffeePrices);
-  // console.log('coffeePriceObj', coffeePriceObj);
+  //create an OBJ of cart Items keyed by price and quantity, regardless of whole bean or ground, to query sanity and calculate total cost
+  const cartObjKeyedByName: Record<string, KeyedCartItem> =
+    reduceCartByName(cart);
 
-  return await createPaymentIntent();
+  // create array of coffeeNames that need to be queried in Sanity
+  const coffeeInCart: string[] = Object.keys(cartObjKeyedByName);
+
+  // query Sanity to verify prices and stock
+  const sanityQuery = `*[_type == "coffee" && name in ${JSON.stringify(
+    coffeeInCart
+  )} && !(_id in path("drafts.**"))] {name, price, stock}
+    `;
+  const currentCoffeePrices: Coffee[] = await sanityClient.fetch(sanityQuery);
+
+  //set retrieved prices onto cartobjKeyedByName
+  currentCoffeePrices.forEach((coffee) => {
+    let key: string = coffee.name;
+    cartObjKeyedByName[key].price = coffee.price;
+  });
+
+  //calculate total with shipping based on verified prices
+  const total = calcVerifiedTotal(cartObjKeyedByName);
+  return await createPaymentIntent(total);
 };
 
 export default function StripeElementsProvider() {
   const paymentIntent = useActionData<typeof action>();
   console.log('paymentIntent', paymentIntent);
-  const [showSummary, toggleShowSummary] = useState(false);
-  const cartItems = useCartItems();
+
   // The stripe prop wants either reference to a fully initialized instance of the Stripe.js client, or a promise that will eventually resolve to one. Luckily, @stripe/stripe-js has a helper for this called loadStripe where we pass in our publishable API key and get back a promise.
   // const options = {
   //   // passing the client secret obtained from the server
@@ -59,42 +63,6 @@ export default function StripeElementsProvider() {
 
   return (
     <>
-      <div className='flex p-2 w-full items-center '>
-        <button
-          className='self-start p-2'
-          onClick={() => toggleShowSummary(!showSummary)}
-        >
-          <div className={`${showSummary ? 'rotate-90' : ''}`}>
-            <PlaySvg />
-          </div>
-        </button>
-        <p>{showSummary ? `Hide Summary` : `Show Summary`}</p>
-        <p className='ml-auto'>T?.00</p>
-      </div>
-      <div>
-        <p className='p-2'>
-          {showSummary ? (
-            <ul className='text-center '>
-              {cartItems.map((cartItem) => (
-                <li className='px-3' key={cartItem.name}>
-                  <p className='flex'>
-                    {`${cartItem.quantity} ${cartItem.name}, ${cartItem.grind}: `}
-                    <span className='ml-auto'>
-                      {`$${formatMoney(cartItem.price * cartItem.quantity)}`}
-                    </span>
-                  </p>
-                </li>
-              ))}
-              <li className='px-3'>
-                <p className='flex'>
-                  Shipping:
-                  <span className='ml-auto'>$?.00</span>
-                </p>
-              </li>
-            </ul>
-          ) : null}
-        </p>
-      </div>
       <Elements
         stripe={stripePromise}
         options={{ clientSecret: paymentIntent.client_secret }}
