61b9155f415126d00bdb18710022816a32684e6e
Corey Hayden <cghayden@gmail.com>
2022-10-02 16:37:52 -0400

    payment element takes payment and routes to success

--- app/routes/pay/index.tsx
+++ app/routes/pay/index.tsx
@@ -1,9 +1,26 @@
 import { Form } from '@remix-run/react';
-import { PaymentElement } from '@stripe/react-stripe-js';
+import {
+  PaymentElement,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
 
-export default function payIndex() {
+export default function Index() {
+  const elements = useElements();
+  const stripe = useStripe();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: 'http://localhost:3000/pay/success',
+      },
+    });
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <PaymentElement />
       <button>confirm payment</button>
     </Form>
