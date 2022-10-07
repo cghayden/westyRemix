77d9f3a8f5d56049e8b4ff27bb507aec87443990
Corey Hayden <cghayden@gmail.com>
2022-10-07 11:52:28 -0400

    stock alert on cart list item if attempt to add more than is available, press of buton will trigger alert, not an add to cart

--- app/components/AdjustQuantityButtons.tsx
+++ app/components/AdjustQuantityButtons.tsx
@@ -1,4 +1,5 @@
 import { CartItem } from 'myTypes';
+import { useState } from 'react';
 import {
   useCartItems,
   useChangeCartItemQuantity,
@@ -12,6 +13,7 @@ export default function AdjustQuantityButtons({
 }: {
   cartItem: CartItem;
 }) {
+  const [alert, setAlert] = useState<string | null>();
   const changeCartItemQuantity = useChangeCartItemQuantity();
   const removeFromCart = useRemoveFromCart();
   const cartItems = useCartItems();
@@ -19,57 +21,64 @@ export default function AdjustQuantityButtons({
   const totalQ = getTotalQuantityInCart(cartItem.coffeeId, cartItems);
 
   return (
-    <div className='flex justify-between items-center'>
-      <div className='rounded-full flex justify-evenly mr-8 bg-green-600 h-9 w-[100px] items-center'>
-        <p>
-          <button
-            className='-mt-1 text-green-50'
-            disabled={cartItem.quantity < 1}
-            onClick={() => {
-              if (cartItem.quantity == 1) {
-                if (confirm(`remove ${cartItem.name} from cart?`)) {
-                  removeFromCart(cartItem);
+    <>
+      <div className='flex justify-between items-center'>
+        <div className='rounded-full flex justify-evenly bg-green-600 h-9 w-[100px] items-center'>
+          <p>
+            <button
+              className='-mt-1 text-green-50'
+              disabled={cartItem.quantity < 1}
+              onClick={() => {
+                if (cartItem.quantity == 1) {
+                  if (confirm(`remove ${cartItem.name} from cart?`)) {
+                    removeFromCart(cartItem);
+                  }
+                  return;
                 }
-                return;
-              }
-              changeCartItemQuantity({
-                name: cartItem.name,
-                coffeeId: cartItem.coffeeId,
-                quantity: -1,
-                grind: cartItem.grind,
-                variant_id: cartItem.variant_id,
-                price: cartItem.price,
-                inStock: cartItem.inStock,
-              });
-            }}
-          >
-            <span className='text-2xl'>-</span>
-          </button>
-        </p>
-        <p className='mx-4 text-xl text-green-50'>{cartItem.quantity}</p>
-        <p>
-          <button
-            disabled={totalQ === cartItem.inStock || cartItem.inStock < totalQ}
-            className='-mt-1 text-green-50'
-            onClick={() =>
-              changeCartItemQuantity({
-                name: cartItem.name,
-                coffeeId: cartItem.coffeeId,
-                quantity: 1,
-                grind: cartItem.grind,
-                variant_id: cartItem.variant_id,
-                price: cartItem.price,
-                inStock: cartItem.inStock,
-              })
-            }
-          >
-            <span className='text-2xl'>+</span>
-          </button>
+                setAlert(null);
+                changeCartItemQuantity({
+                  name: cartItem.name,
+                  coffeeId: cartItem.coffeeId,
+                  quantity: -1,
+                  grind: cartItem.grind,
+                  variant_id: cartItem.variant_id,
+                  price: cartItem.price,
+                  inStock: cartItem.inStock,
+                });
+              }}
+            >
+              <span className='text-2xl'>-</span>
+            </button>
+          </p>
+          <p className='mx-4 text-xl text-green-50'>{cartItem.quantity}</p>
+          <p>
+            <button
+              className='-mt-1 text-green-50'
+              onClick={() => {
+                if (totalQ === cartItem.inStock || cartItem.inStock < totalQ) {
+                  setAlert(`There are only ${cartItem.inStock} available`);
+                } else {
+                  changeCartItemQuantity({
+                    name: cartItem.name,
+                    coffeeId: cartItem.coffeeId,
+                    quantity: 1,
+                    grind: cartItem.grind,
+                    variant_id: cartItem.variant_id,
+                    price: cartItem.price,
+                    inStock: cartItem.inStock,
+                  });
+                }
+              }}
+            >
+              <span className='text-2xl'>+</span>
+            </button>
+          </p>
+        </div>
+        {alert && <p>{alert}</p>}
+        <p className='text-xl'>
+          {`$${formatMoney(cartItem.price * cartItem.quantity)} `}
         </p>
       </div>
-      <p className='text-xl'>
-        {`$${formatMoney(cartItem.price * cartItem.quantity)} `}
-      </p>
-    </div>
+    </>
   );
 }
