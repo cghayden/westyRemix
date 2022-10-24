1a358ad61a9ae4d1ab95863db1ac31a541242cef
Corey Hayden <cghayden@gmail.com>
2022-10-24 11:20:06 -0400

    padding-left on InputRow

--- app/components/styledContainers/CheckoutTextInputRow.tsx.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CheckoutTextInputRow({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className='text-sm '>{children}</div>;
-}
--- app/components/styledContainers/InputRow.tsx
+++ app/components/styledContainers/InputRow.tsx
@@ -1,7 +1,7 @@
 export default function InputRow({ children }: { children: React.ReactNode }) {
   return (
     //font-size: 1.1rem?
-    <div className='label__input-row w-full py-3 pr-3 bg-blue-50 items-center flex'>
+    <div className='label__input-row w-full py-3 px-3  bg-blue-50 items-center flex'>
       {children}
     </div>
   );
